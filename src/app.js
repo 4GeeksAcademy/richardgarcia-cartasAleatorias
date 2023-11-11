7d45c7d2b8f665a01b11309e90f19870938af07a
richardgarciaordaz <64115217+richardgarciaordaz@users.noreply.github.com>
2023-11-11 17:11:25 +0000

    cartasAleatorias

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♥", "♠", "♣"];
+  let num = [2, 3, 4, 5, 6, 7, 8, 9, "A", "J", "Q", "K"];
+  let paloRandom = palos[Math.floor(Math.random() * palos.length)];
+
+  let numRandom = num[Math.floor(Math.random() * num.length)];
+  console.log(paloRandom, numRandom);
+  document.getElementById("paloArriba").innerHTML = paloRandom;
+  document.getElementById("paloAbajo").innerHTML = paloRandom;
+  document.getElementById("numero").innerHTML = numRandom;
+
+  if (paloRandom == "♥" || paloRandom == "♦") {
+    document.getElementById("paloArriba").style.color = "red";
+    document.getElementById("paloAbajo").style.color = "red";
+  }
 };
